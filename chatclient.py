48ff86983c336b0ddb92871bc430eb8ab84f076b
Ingrid Avendaño <ingridavendano@gmail.com>
2013-10-23 17:51:32 -0700

    Local chat client.

---
+++ chatclient.py
@@ -0,0 +1,33 @@
+import socket
+import sys
+import select
+
+my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+my_socket.connect(("localhost", 5555))
+
+data = my_socket.recv(1024)
+print "received:\n%s" %  data
+
+user_name = sys.stdin.readline().strip()
+my_socket.send(user_name)
+data = my_socket.recv(1024)
+print data
+
+running = True
+while running:
+    inputready, outputready, exceptready = select.select([my_socket, sys.stdin], [], [])
+
+    for s in inputready:
+        if s == my_socket:
+            msg = s.recv(1024)
+            if msg:
+                print msg
+            else:
+                print "Disconnected from Server!"
+                running = False
+        if s == sys.stdin:
+            msg = sys.stdin.readline()
+            my_socket.sendall(msg)
+
+
+my_socket.close()
